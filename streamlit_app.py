d0fa9a0d5cff099ba213ca9b1e0422c8ba38a10c
terzatel <61910797+terzatell@users.noreply.github.com>
2024-05-03 17:25:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Загрузка данных
+@st.cache
+def load_data(url):
+    return pd.read_csv(url)
+
+data_url = "https://github.com/terzatell/terzatel/edit/main/streamlit_app.py"
+data = load_data(data_url)
+
+# Визуализация данных
+st.title('Визуализация данных о странах Центральной Азии')
+
+fig = px.bar(data, x='Страна', y='Показатель', title='Показатели по странам')
+st.plotly_chart(fig)
 
